b8c3e4c40b7c9d91df09f7eb5a5c4e2bbf17ca0b
reactK1n <adewings1995@gmail.com>
2023-10-28 04:40:41 -0700

    Create: Level-8-video-8

---
+++ Assets/Scripts/Enums/PieceType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Enums
+{
+	public enum PieceType
+	{
+		none = -1,
+		ramp = 0,
+		longblock = 1,
+		jump = 2,
+		slide = 3
+	}
+}
---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,10 @@
+using Assets.Scripts.Enums;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+	public PieceType type;
+	public int visualIndex;
+}
